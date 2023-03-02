7a510fba1145373e525ede90fec919759417b992
NickyVamp <nickimonster@hotmail.com>
2023-03-02 12:45:26 -0600

    TERMINE unu

---
+++ EXAMEN_PARCIAL_LOPEZ_ANNA/Assets/Scrips/JuegoDeSumas.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class JuegoDeSumas : MonoBehaviour
+{
+    private int operacion; 
+    private int numero;
+    public TMP_InputField input;
+    public TMP_Text text;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerarEcuacionSuma();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    } 
+
+
+   /*
+    private void Ecuacion()
+    {
+        int.TryParse(input.text, out);
+
+        if ( )
+        {
+            text.text = ""
+        }
+    } */ 
+
+}
